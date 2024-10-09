5ebe2922dab56da15ba5b7d4aaa9e51ad62310e1
Felipe Moreira <felipe0365@gmail.com>
2024-10-09 10:41:00 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/compassuol/course/entities/Category.java
+++ src/main/java/com/compassuol/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.compassuol.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -21,7 +22,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
