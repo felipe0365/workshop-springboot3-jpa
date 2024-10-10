9116946be53ef0d015ac0ddedb1144b0c79c08ab
Felipe Moreira <felipe0365@gmail.com>
2024-10-10 16:41:04 -0300

    Exception handling - update

--- src/main/java/com/compassuol/course/services/UserService.java
+++ src/main/java/com/compassuol/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.compassuol.course.entities.User;
 import com.compassuol.course.repositories.UserRepository;
 import com.compassuol.course.services.exceptions.DatabaseException;
 import com.compassuol.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User user) {
