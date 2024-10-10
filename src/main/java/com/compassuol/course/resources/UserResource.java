612076734e230078f99e5b3ba569d2bea77b467f
Felipe Moreira <felipe0365@gmail.com>
2024-10-10 11:29:16 -0300

    User update

--- src/main/java/com/compassuol/course/resources/UserResource.java
+++ src/main/java/com/compassuol/course/resources/UserResource.java
@@ -42,4 +42,9 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
 }
